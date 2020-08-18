c0fea1b32249cd035e35a8f5988ceadf3a58907c
Binu Manoharan <binu.manoharan@i-nexus.com>
2020-08-19 00:34:31 +0100

    Tidy up
    Refactor method and add inline doc

--- src/main/java/learn/binu/Main.java
+++ src/main/java/learn/binu/Main.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 
 public class Main {
 
+    // List of available pack sizes to place order for
     public static int[] PACK_SIZES = {250, 500, 1000, 2000, 5000};
-    private static Scanner scanner = new Scanner(System.in);
     private static SweetShop sweetShop = new SweetShop();
+
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         final String order = getOrderSize();
         final int orderSize = Integer.parseInt(order);
 
         final List<Pack> packs = sweetShop.placeOrder(PACK_SIZES, orderSize);
+        printOrder(packs);
+    }
+
+    private static void printOrder(List<Pack> packs) {
         System.out.println("Generated Order with the below details");
         for (Pack pack : packs) {
             System.out.println(pack);
